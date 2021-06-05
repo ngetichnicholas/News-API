5e8236bfb78ea0441eb784367df34730442dcc0e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-05 16:22:10 +0300

    Initialize bootstrap and register the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,22 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import 
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+  app = Flask(__name__)
+
+  #Create the app configuration
+  app.config.from_object(config_options[config_name])
+
+  #INitializing flask extensions
+  bootstrap.init_app(app)
+
+  #Rejistering the blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+  #Setting config
+  
