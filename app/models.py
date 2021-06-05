88798e73346817d37276fee642ff1fbfcbaf05d7
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-05 18:31:11 +0300

    Add class model for news article

--- app/models.py
+++ app/models.py
@@ -3,4 +3,11 @@ class News:
   News class to define news objects
   '''
 
-  def __init__(self):
+  def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
+    self.author =author
+    self.title = title
+    self.description = description
+    self.url = url
+    self.urlToImage = urlToImage
+    self.puplishedAt = publishedAt
+    self.content = content
