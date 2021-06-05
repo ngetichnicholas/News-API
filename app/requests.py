7d3af50de946a007de818adfe2027ba54c093599
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-05 18:45:56 +0300

    Add api key and base url configuration to requests

--- app/requests.py
+++ app/requests.py
@@ -1,2 +1,13 @@
 import urllib.request,json
-from 
+from .models import News
+
+#Getting api key
+api_key = None
+
+#Getting the news base url
+base_url = None
+
+def configure_request(app):
+  global api_key,base_url
+  api_key = app.config['NEWS_API_KEY']
+  base_url = app.config['NEWS_API_BASE_URL']
